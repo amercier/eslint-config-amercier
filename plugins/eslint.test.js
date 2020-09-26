049631c0c32bf6d7860f4e6b4e6d519f994677c8
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-09-26 12:38:48 +0200

    Add test helpers

--- plugins/eslint.test.js
+++ plugins/eslint.test.js
@@ -1,100 +1,61 @@
-const { readdirSync } = require('fs')
-const { dirname } = require('path')
-const airbnb = require('eslint-config-airbnb-base')
-const prettier = require('eslint-config-prettier')
-const standard = require('eslint-config-standard')
-const eslint = require('./eslint')
-const { mapValues } = require('lodash')
-
-const { CascadingConfigArrayFactory } = require('@eslint/eslintrc').Legacy
-
-function normalizeRule(rule) {
-  if (Array.isArray(rule) && rule.length === 1) {
-    return normalizeRule(rule[0])
-  }
-  if (rule === 0) {
-    return 'off'
-  }
-  return rule
-}
-
-const [airbnbRules, prettierRules, standardRules] = [
-  airbnb,
-  prettier,
-  standard,
-].map((baseConfig) => {
-  const { rules } = new CascadingConfigArrayFactory({ baseConfig })
-    .getConfigArrayForFile('index.js')
-    .extractConfig()
-
-  return mapValues(rules, normalizeRule)
-})
-
-const ESLINT_RULE_NAMES = readdirSync(
-  dirname(require.resolve('eslint/lib/rules')),
-)
-  .filter((name) => !['utils', 'index.js'].includes(name))
-  .map((name) => name.replace(/\.js$/, ''))
+const {
+  eslintBaseRuleNames,
+  airbnbConfigRules,
+  prettierConfigRules,
+  standardConfigRules,
+} = require('../test/helpers')
+const { rules } = require('./eslint')
 
 it('are alphabetically ordered', () => {
   const compare = (a, b) => (a < b ? -1 : 1)
-  expect(Object.keys(eslint.rules)).toStrictEqual(
-    Object.keys(eslint.rules).sort(compare),
-  )
+  expect(Object.keys(rules)).toStrictEqual(Object.keys(rules).sort(compare))
 })
 
-describe.each(ESLINT_RULE_NAMES)('%s rule', (ruleName) => {
+describe.each(eslintBaseRuleNames)('%s rule', (ruleName) => {
   it('is configured', () => {
-    expect(eslint.rules).toHaveProperty(ruleName)
+    expect(rules).toHaveProperty(ruleName)
   })
 })
 
-describe.each(Object.entries(eslint.rules))(
-  '%s definition',
-  (ruleName, rule) => {
-    it('matches an existing rule', () => {
-      expect(ESLINT_RULE_NAMES).toContain(ruleName)
+const AIRBNB_ENABLED_RULES = [
+  'default-case-last',
+  'default-param-last',
+  'grouped-accessor-pairs',
+  'no-constructor-return',
+  'no-dupe-else-if',
+  'no-import-assign',
+  'no-loss-of-precision',
+  'no-promise-executor-return',
+  'no-restricted-exports',
+  'no-setter-return',
+  'no-unreachable-loop',
+  'no-useless-backreference',
+  'prefer-exponentiation-operator',
+]
+
+describe.each(Object.entries(rules))('%s definition', (ruleName, rule) => {
+  it('matches an existing rule', () => {
+    expect(eslintBaseRuleNames).toContain(ruleName)
+  })
+
+  if (prettierConfigRules[ruleName]) {
+    it('follows Prettier rule', async () => {
+      expect(rule).toStrictEqual(prettierConfigRules[ruleName])
     })
-
-    if (prettierRules[ruleName]) {
-      it('follows Prettier rule', async () => {
-        expect(rule).toStrictEqual(prettierRules[ruleName])
-      })
-    } else if (standardRules[ruleName]) {
-      it('follows Standard rule', async () => {
-        expect(rule).toStrictEqual(standardRules[ruleName])
-      })
-    } else if (airbnbRules[ruleName]) {
-      const ENABLED_RULES = [
-        'default-case-last',
-        'default-param-last',
-        'grouped-accessor-pairs',
-        'no-constructor-return',
-        'no-dupe-else-if',
-        'no-import-assign',
-        'no-loss-of-precision',
-        'no-promise-executor-return',
-        'no-restricted-exports',
-        'no-setter-return',
-        'no-unreachable-loop',
-        'no-useless-backreference',
-        'prefer-exponentiation-operator',
-      ]
-
-      if (ENABLED_RULES.includes(ruleName)) {
-        it('follows Airbnb rule but is enabled', async () => {
-          expect(rule).not.toStrictEqual(airbnbRules[ruleName])
-          expect(rule).toStrictEqual(
-            Array.isArray(airbnbRules[ruleName])
-              ? ['error', ...airbnbRules[ruleName].slice(1)]
-              : 'error',
-          )
-        })
-      } else {
-        it('follows Airbnb rule', async () => {
-          expect(rule).toStrictEqual(airbnbRules[ruleName])
-        })
-      }
-    }
-  },
-)
+  } else if (standardConfigRules[ruleName]) {
+    it('follows Standard rule', async () => {
+      expect(rule).toStrictEqual(standardConfigRules[ruleName])
+    })
+  } else if (
+    airbnbConfigRules[ruleName] &&
+    AIRBNB_ENABLED_RULES.includes(ruleName)
+  ) {
+    it('follows Airbnb rule', async () => {
+      expect(rule).toStrictEqual(airbnbConfigRules[ruleName])
+    })
+  } else if (!airbnbConfigRules[ruleName]) {
+    it('is OFF', async () => {
+      expect(rule).toStrictEqual('off')
+    })
+  }
+})
---
+++ test/helpers.js
@@ -0,0 +1,48 @@
+const { CascadingConfigArrayFactory } = require('@eslint/eslintrc').Legacy
+const { mapValues } = require('lodash')
+
+function normalizeRule(rule) {
+  if (Array.isArray(rule) && rule.length === 1) {
+    return normalizeRule(rule[0])
+  }
+  if (rule === 0) {
+    return 'off'
+  }
+  return rule
+}
+
+function getPluginRuleNames(pluginName) {
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  const { rules } = require(`eslint-plugin-${pluginName}`)
+  return Object.keys(rules).map((ruleName) => `${pluginName}/${ruleName}`)
+}
+
+function getConfigRules(...configNames) {
+  const baseConfig = { extends: configNames }
+  const configArrayFactory = new CascadingConfigArrayFactory({
+    baseConfig,
+  })
+    .getConfigArrayForFile(`${__dirname}/../index.js`)
+    .extractConfig()
+
+  return mapValues(configArrayFactory.rules, normalizeRule)
+}
+
+module.exports = {
+  eslintBaseRuleNames:
+    // eslint-disable-next-line global-require
+    [...require('eslint/lib/rules').keys()],
+  importPluginRuleNames: getPluginRuleNames('import'),
+  airbnbConfigRules: getConfigRules('airbnb'),
+  prettierConfigRules: getConfigRules(
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/babel',
+    'prettier/flowtype',
+    'prettier/react',
+    'prettier/standard',
+    'prettier/unicorn',
+    'prettier/vue',
+  ),
+  standardConfigRules: getConfigRules('standard'),
+}
