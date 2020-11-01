3a27607eb64ef967eb315d0b339d4a885a4b3392
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-11-01 15:56:33 +0100

    Improve Storybook support in create-react-app config

--- create-react-app.js
+++ create-react-app.js
@@ -1,16 +1,29 @@
 module.exports = {
   overrides: [
-    { files: 'public/**', extends: './browser' },
-    { files: 'src/**', extends: './react' },
-    { files: 'src/**/*.stories.js', extends: './storybook' },
     {
-      files: ['!{public,src}/**', 'src/setupTests.js'],
-      extends: './node',
-      globals: { global: true },
-      rules: { 'import/no-extraneous-dependencies': 'off' },
+      files: ['**/*'],
+      excludedFiles: ['public/**', 'src/**'],
+      extends: ['./node', './dev'],
     },
     {
-      files: 'src/serviceWorker.js',
+      files: ['scripts/**/*'],
+      extends: ['./node', './dev'],
+    },
+
+    { files: ['public/**'], extends: './browser' },
+
+    {
+      files: ['src/**'],
+      extends: './react',
+    },
+    { files: ['src/**/*.stories.js'], extends: ['./dev', './storybook'] },
+    {
+      files: ['src/setupTests.js'],
+      extends: ['./dev'],
+      globals: { global: true },
+    },
+    {
+      files: ['src/serviceWorker.js'],
       globals: { process: true },
       rules: { 'no-console': 'off' },
     },
